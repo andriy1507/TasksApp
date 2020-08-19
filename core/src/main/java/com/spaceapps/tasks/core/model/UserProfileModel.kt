eb38064b46f80ff75879e21fb165fcf85467f67c
Andriy Goryachok <andriy1507@hotmail.com>
2020-08-19 17:53:11 +0300

    Fixed error on profile page

--- core/src/main/java/com/spaceapps/tasks/core/model/UserProfileModel.kt
+++ core/src/main/java/com/spaceapps/tasks/core/model/UserProfileModel.kt
@@ -3,7 +3,7 @@ package com.spaceapps.tasks.core.model
 import java.util.*
 
 data class UserProfileModel(
-    val id: UUID,
+    val id: UUID?,
     val userName: String,
     val profileImage: String
 ) : DomainEntity
--- repository/src/main/java/com/spaceapps/tasks/repository/mapper/UserProfileMapper.kt
+++ repository/src/main/java/com/spaceapps/tasks/repository/mapper/UserProfileMapper.kt
@@ -1,13 +1,14 @@
 package com.spaceapps.tasks.repository.mapper
 
+import android.util.Log
 import com.spaceapps.tasks.core.model.UserProfileModel
 import com.spaceapps.tasks.remote.model.UserProfileModelRemote
 import java.util.*
 
-fun UserProfileModel.toUserProfileRemote() :UserProfileModelRemote{
+fun UserProfileModel.toUserProfileRemote(): UserProfileModelRemote {
     return UserProfileModelRemote(id, userName, profileImage)
 }
 
-fun UserProfileModelRemote.toUserProfileModel():UserProfileModel{
-    return UserProfileModel(UUID.fromString(id.toString()), userName.orEmpty(), profileImage.orEmpty())
+fun UserProfileModelRemote.toUserProfileModel(): UserProfileModel {
+    return UserProfileModel(id, userName.orEmpty(), profileImage.orEmpty())
 }
