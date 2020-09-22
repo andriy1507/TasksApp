73779bfd3719ab7349002d4a87479bb95e8902ab
Andriy Goryachok <andriy1507@hotmail.com>
2020-09-22 18:00:03 +0300

    Delete .space.kts

--- .space.kts
+++
@@ -1,9 +0,0 @@
-/**
-* JetBrains Space Automation
-* This Kotlin-script file lets you automate build activities
-* For more info, refer to https://www.jetbrains.com/help/space/automation.html
-*/
-
-job("Build Android Debug") {
-    gradlew("openjdk:11","assembleDebug")
-}
