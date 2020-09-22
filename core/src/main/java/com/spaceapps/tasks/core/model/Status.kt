a14f74fff21094a2b329506ef7b7166968e5adc7
Andriy Goryachok <andriy1507@hotmail.com>
2020-09-22 19:23:39 +0300

    Improved error handling

--- core/src/main/java/com/spaceapps/tasks/core/model/Status.kt
+++ core/src/main/java/com/spaceapps/tasks/core/model/Status.kt
@@ -1,6 +1,16 @@
 package com.spaceapps.tasks.core.model
 
 sealed class Status<out T> {
+
+    companion object {
+
+        fun loading() = Loading
+
+        fun <T> success(data: T) = Success(data)
+
+        fun error(error: Exception) = Error(error)
+    }
+
     object Loading : Status<Nothing>()
     class Success<out T>(val data: T) : Status<T>()
     class Error(val error: Exception) : Status<Nothing>()
